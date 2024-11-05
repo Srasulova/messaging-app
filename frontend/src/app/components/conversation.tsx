18523868770724c2cb99614c3edee0540a9c131d
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-11-05 14:47:15 -0500

    add form and conversation

---
+++ frontend/src/app/components/conversation.tsx
@@ -0,0 +1,8 @@
+export default function Conversation() {
+    return (
+        <>
+            <div className="w-1/2 h-96 mb-10 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300" >
+                <p className="">this is conversation </p></div>
+        </>
+    )
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+import Conversation from "./components/conversation";
+import InputText from "./components/form";
+
 export default function Home() {
   return (
     <div className="">
       <main className="">
-
+        <Conversation />
+        <InputText />
       </main>
       <footer className="">
 
