f7401e03c6dd27186fd1295e44e59ad8c8dc9a12
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-11-05 23:42:09 -0500

    add conversation window

--- frontend/src/app/components/conversation.tsx
+++ frontend/src/app/components/conversation.tsx
@@ -3,7 +3,7 @@ import { ConversationProps } from "../page"
 export default function Conversation({ messages }: ConversationProps) {
     return (
         <>
-            <div className="w-1/2 h-96 mb-10 p-4 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300" >
+            <div className="w-1/2 h-48 mb-10 p-4 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300" >
                 {messages.map((msg, index) => (
                     <p key={index} className="mb-2 text-gray-800">{msg}</p>
                 ))}
